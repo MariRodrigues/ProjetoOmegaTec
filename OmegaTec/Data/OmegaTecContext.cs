452701e0daa56443f6d484eccb793b9cb29d51a8
Mariana Rodrigues <46384658+MariRodrigues@users.noreply.github.com>
2021-05-22 22:59:50 -0300

    primeiro commit

---
+++ OmegaTec/Data/OmegaTecContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OmegaTec.Models;
+
+namespace OmegaTec.Data
+{
+    public class OmegaTecContext : DbContext
+    {
+        public OmegaTecContext (DbContextOptions<OmegaTecContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OmegaTec.Models.Cliente> Cliente { get; set; }
+    }
+}
